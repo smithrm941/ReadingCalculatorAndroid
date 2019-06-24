0b66e0ed96820141b89b11073132efefd8b54ab9
smithrm941 <smithrm941@gmail.com>
2019-06-23 19:08:59 -0700

    User input saved as strings and ints

--- app/src/main/java/com/rhondasmith/readingcalculator/MainActivity.java
+++ app/src/main/java/com/rhondasmith/readingcalculator/MainActivity.java
@@ -3,12 +3,42 @@ package com.rhondasmith.readingcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText bookTitle;
+    private EditText numberOfPages;
+    private EditText currentPage;
+    private EditText daysToFinish;
+    private Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookTitle = findViewById(R.id.whatBook);
+        numberOfPages = findViewById(R.id.numberOfPages);
+        currentPage = findViewById(R.id.currentPage);
+        daysToFinish = findViewById(R.id.daysToFinish);
+        calculateButton = findViewById(R.id.calculateButton);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = bookTitle.getText().toString();
+                String bookLength = numberOfPages.getText().toString();
+                String startingPage = currentPage.getText().toString();
+                String numberOfDays = daysToFinish.getText().toString();
+
+                int numberOfPages = Integer.parseInt(bookLength);
+                int currentPage = Integer.parseInt(startingPage);
+                int daysToFinish = Integer.parseInt(numberOfDays);
+            }
+        });
     }
 }
